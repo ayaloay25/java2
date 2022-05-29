355b6f7473f983d16df826dee1040e1f317a43ac
AYA LUAY <ayaloay898@gmail.com>
2022-05-29 22:49:27 +0300

    complete Task7

---
+++ task7/style/js/js7.js
@@ -0,0 +1,6 @@
+let a = "Ahmed Ali";
+console.log(a[0].toLowerCase());
+console.log(a.substring(1, -1).toLowerCase())
+console.log(a.slice(0, 1).toLowerCase());
+console.log(a.substr(0, 1).toLowerCase());
+console.log(a.charAt(0).toLowerCase());
